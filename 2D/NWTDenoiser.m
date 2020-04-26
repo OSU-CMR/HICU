a02ef812159d6a8f7bd1e13c24ae4d6dcf280582
Shen Zhao <62859186+Zhao-Shen@users.noreply.github.com>
2020-04-26 04:01:09 -0400

    Update NWTDenoiser.m

--- 2D/NWTDenoiser.m
+++ 2D/NWTDenoiser.m
@@ -1,4 +1,4 @@
-function Kdata = NWTDenoiser(Kdata,lam1,lam2) % NWT denoiser
+function Kdata = NWTDenoiser(Kdata,Lam1,Lam2) % NWT denoiser
 I = K2I(Kdata);
 c = max(abs(I),[],'all'); % the max absolute value
 I = I/c;                  % normalize
@@ -10,10 +10,10 @@ I_LH = convn(I_cp,[1 1; -1 -1]/4,'valid');
 I_HL = convn(I_cp,[1 -1; 1 -1]/4,'valid');
 I_HH = convn(I_cp,[1 -1;-1 1]/4,'valid');
 
-I_LL = max(abs(I_LL)-lam1,0).*exp(1j*angle(I_LL));
-I_LH = max(abs(I_LH)-lam2,0).*exp(1j*angle(I_LH));
-I_HL = max(abs(I_HL)-lam2,0).*exp(1j*angle(I_HL));
-I_HH = max(abs(I_HH)-lam2,0).*exp(1j*angle(I_HH));
+I_LL = max(abs(I_LL)-Lam1,0).*exp(1j*angle(I_LL));
+I_LH = max(abs(I_LH)-Lam2,0).*exp(1j*angle(I_LH));
+I_HL = max(abs(I_HL)-Lam2,0).*exp(1j*angle(I_HL));
+I_HH = max(abs(I_HH)-Lam2,0).*exp(1j*angle(I_HH));
 
 I = I_LL + I_LH + I_HL + I_HH;
 I = I*c;
