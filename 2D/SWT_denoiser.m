a94670f5a1f86bb2adb80e90cf2c69b000501ef1
Shen Zhao <62859186+Zhao-Shen@users.noreply.github.com>
2020-05-03 02:52:09 -0400

    Update SWT_denoiser.m
    Modify the normalization step based on k-space instead of image domain.
    
    Give another highly vectorized version of calculating different bands using FFT.

--- 2D/SWT_denoiser.m
+++ 2D/SWT_denoiser.m
@@ -1,4 +1,4 @@
-function Kdata = SWT_denoiser(Kdata,Step_size,Lam_1,Lam_2) 
+function Kdata = SWT_denoiser(Kdata,Step_size,Lam_1,Lam_2)
 % This function provide capability of regularizing the least square with
 % stationary wavelet transform (SWT), which is similar to proximal operator
 % See the accompanying license.txt for additional license information.
@@ -12,34 +12,41 @@ function Kdata = SWT_denoiser(Kdata,Step_size,Lam_1,Lam_2)
 % Output ------------------------------------------------------------------
 % Kdata:     k-space data                                   (tensor: #kx x #ky x #coil)
 
-
-I = K2I(Kdata);
-c = max(abs(I),[],'all');% the max absolute value
-I = I/c;                 % normalize
-I_cp = padarray(I,[1,1,0],'circular','pre');
-
-% Generate station wavelet bands
-I_LL = convn(I_cp,[1 1; 1 1]/4,'valid');
-I_LH = convn(I_cp,[1 1; -1 -1]/4,'valid');
-I_HL = convn(I_cp,[1 -1; 1 -1]/4,'valid');
-I_HH = convn(I_cp,[1 -1;-1 1]/4,'valid');
-
-% Soft threshold each band
-I_LL = max(abs(I_LL)-Lam_1*abs(Step_size),0).*exp(1j*angle(I_LL));
-I_LH = max(abs(I_LH)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_LH));
-I_HL = max(abs(I_HL)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_HL));
-I_HH = max(abs(I_HH)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_HH));
-
-I = I_LL + I_LH + I_HL + I_HH;
-I = I*c;
-
-Kdata = I2D(I);
+switch 1
+    case 1 % direct convolution to calculate the soft-thresholding, lower flops but sequential, slower when wavelet basis is large
+        I = K2I(Kdata);
+        c = max(abs(Kdata),[],'all');                                                                                                     % the largest absolute value in k-space, for normalizzation        
+        I = I/c;                 % normalize
+        I_cp = padarray(I,[1,1,0],'circular','pre');
+        
+        %Generate stationary wavelet bands
+        I_LL = convn(I_cp,[1  1;  1  1]/4,'valid');
+        I_LH = convn(I_cp,[1  1; -1 -1]/4,'valid');
+        I_HL = convn(I_cp,[1 -1;  1 -1]/4,'valid');
+        I_HH = convn(I_cp,[1 -1; -1  1]/4,'valid');
+        
+        % Soft threshold each band
+        I_LL = max(abs(I_LL)-Lam_1*abs(Step_size),0).*exp(1j*angle(I_LL));
+        I_LH = max(abs(I_LH)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_LH));
+        I_HL = max(abs(I_HL)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_HL));
+        I_HH = max(abs(I_HH)-Lam_2*abs(Step_size),0).*exp(1j*angle(I_HH));
+        
+        I = I_LL + I_LH + I_HL + I_HH;
+        I = I*c;
+        
+        Kdata = I2K(I);                                                                                                                   % back to  k-space        
+    case 2 % FFT based convolution to calculate the soft thersholding, highly vectorized, speed is slower than direct convolution but does not change when wavelet baisis is large                      
+        c = max(abs(Kdata),[],'all');                                                                                                     % the largest absolute value in k-space, for normalizzation        
+        I_bands = ifft2(fft2(K2I(Kdata)).*fft2(cat(4,[1 1; 1  1],[1 1; -1 -1],[1 -1; 1 -1],[1 -1; -1 1])/4,size(Kdata,1),size(Kdata,2))); % generate stationary wavelet bands LL LH HL HH                
+        I_bands = max(abs(I_bands)-reshape([Lam_1, Lam_2, Lam_2, Lam_2]*abs(Step_size)*c,1,1,1,4), 0).*exp(1j*angle(I_bands));            % soft threshold each band
+        Kdata = I2K(sum(I_bands,4)); % back to  k-space
+end
 end
 
-function I = K2I(Kdata)   % k-space to image domain
+function I = K2I(Kdata)                                                                                                                   % k-space to image domain
 I = sqrt(size(Kdata,1)*size(Kdata,2))*fftshift(fftshift(ifft(ifft(ifftshift(ifftshift(Kdata,1),2),[],1),[],2),1),2);
 end
 
-function D = I2D(I)       % image domain to k-sapce
-D = 1/sqrt(size(I,1)*size(I,2))*fftshift(fftshift(fft(fft(ifftshift(ifftshift(I,2),1),[],2),[],1),2),1);
+function Kdata = I2K(I)                                                                                                                   % image domain to k-sapce
+Kdata = 1/sqrt(size(I,1)*size(I,2))*fftshift(fftshift(fft(fft(ifftshift(ifftshift(I,2),1),[],2),[],1),2),1);
 end
